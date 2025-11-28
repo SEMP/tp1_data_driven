71254126e3aa2169251d3a8d42f0a8146a7ad1c5
Sergio Morel <sergiomorel@gmail.com>
2025-11-28 06:01:41 -0300

    query de mapa con limite de 1000 registros para evitar sobrecarga de grafico.

--- grafana/graficos.sql
+++ grafana/graficos.sql
@@ -1,8 +1,31 @@
--- Grafico de accidentes diarios
+-- Gráfico de accidentes diarios
 SELECT
   unixepoch(accidents_t.date) AS time,
   accidents_t.accidents_count
 FROM accidents_daily AS accidents_t
-WHERE unixepoch(accidents_t.date) BETWEEN $__from/1000 AND $__to/1000
+WHERE time BETWEEN $__from/1000 AND $__to/1000
 ORDER BY time
 ;
+
+-- Mapa de accidentes
+SELECT
+  unixepoch(accident_t.date) AS time,
+  accident_t.uf,
+  accident_t.municipio,
+  accident_t.latitude,
+  accident_t.longitude
+FROM accidents_spatial AS accident_t
+WHERE time BETWEEN $__from/1000 AND $__to/1000
+;
+
+-- Mapa de accidentes, muestras aleatorias de 1000 registros (para no sobrecargar el grafico)
+SELECT
+	unixepoch(accident_t.date) AS time,
+	accident_t.uf,
+	accident_t.municipio,
+	accident_t.latitude,
+	accident_t.longitude
+FROM accidents_spatial AS accident_t
+WHERE time BETWEEN $__from/1000 AND $__to/1000
+ORDER BY RANDOM()
+LIMIT 1000;
